58e59c61d14810b8c4d3a8bf8dd5dafa8d617efc
adamrbell <adamrbell@gmail.com>
2016-06-25 00:03:32 +0100

    Update startapp.sh

--- src/startapp.sh
+++ src/startapp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install python libs from requirements
-pip install -r /code/requirements.txt && \
+pip install -r /code/requirements.txt
 
 # run Main code block
 python /code/main.py
